e281da6379af141bd3881783f9f62f4cd61580a3
fabioliveirasch <fabioscholiv@gmail.com>
2023-04-25 12:13:07 -0300

    Primeira classe: Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3, 5);
+		
+		System.out.println(pos);
 
-		System.out.println("Fabio");
 		
 	}
 
